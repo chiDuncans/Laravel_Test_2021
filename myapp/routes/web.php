d092e77b4ce1a5edf11fa5ac793d22da4313a800
chiDuncans <chiekoduncans@gmail.com>
2021-08-15 00:54:32 -0700

    Added laravel debugger, laravel ui, bootstrap. Modified locale and timezone in config app file.

--- myapp/routes/web.php
+++ myapp/routes/web.php
@@ -16,3 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    return "About page";
+});
+
+Route::get('/contact', function () {
+    return "Contact page";
+});
+
+Route::get('/post/{id}', function($id){
+    return "post #" . $id;
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
